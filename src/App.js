ced088185dd05359e53b296708e13fc30a6f204f
abhaykumar00 <125191416+abhaykumar00@users.noreply.github.com>
2024-04-11 17:43:18 +0530

    a

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Toaster } from "sonner";
+import { AppProvidersWrapper } from "./components";
+import AllRoutes from "./routes/Routes";
+import { configureFakeBackend } from "@/common";
+
+// styles
+import "@/assets/css/style.css";
+
+configureFakeBackend();
+
+const App = () => {
+  return (
+    <AppProvidersWrapper>
+      <AllRoutes />
+      
+      
+    </AppProvidersWrapper>
+  );
+};
+
+export default App;
